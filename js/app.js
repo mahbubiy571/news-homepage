d0e9c5b93a0a2dc202de1d7bde488501364a6c9a
avazbek <avazbek@gmail.com>
2025-05-13 23:31:25 +0500

    ...

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 const showBtn = document.querySelector('#show-btn');
 const hiddenNav = document.querySelector(".header__hidden");
+const showBtnImage = document.querySelector("#show-btn__image");
 
+let ChangeImage = false;
 showBtn.addEventListener("click", () => {
     hiddenNav.classList.toggle("show");
     document.body.style.overflowY = "hidden";
+    if(ChangeImage) {
+        console.log(ChangeImage);
+        showBtnImage.src = "../assets/menu.svg" ;
+    } else {
+        console.log(ChangeImage);
+        showBtnImage.src = "../assets/pic-menu.svg" ;
+    }
+
+    ChangeImage = !ChangeImage;
 });
